ed5eb44d4d2e884c6f00e051bfa576c9d1cd8d27
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2019-08-22 16:10:48 -0500

    Updates

---
+++ precache-manifest.ff26736dba32d1eb26d878d57e9d4030.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be7c304c84a9af42b59fee31387b1cea",
+    "url": "/Lapate-ada/index.html"
+  },
+  {
+    "revision": "c4af9b97246a94ec9b13",
+    "url": "/Lapate-ada/static/css/2.2f374adb.chunk.css"
+  },
+  {
+    "revision": "e1b6f34a88a778037c70",
+    "url": "/Lapate-ada/static/css/main.4df74037.chunk.css"
+  },
+  {
+    "revision": "c4af9b97246a94ec9b13",
+    "url": "/Lapate-ada/static/js/2.7b5be516.chunk.js"
+  },
+  {
+    "revision": "e1b6f34a88a778037c70",
+    "url": "/Lapate-ada/static/js/main.025f350f.chunk.js"
+  },
+  {
+    "revision": "f5669c6dc74806af50dd",
+    "url": "/Lapate-ada/static/js/runtime~main.73e51985.js"
+  }
+]);
